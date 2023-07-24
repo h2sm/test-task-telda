3abc1d52de5882b769512159bb2d3d039327f4dd
Egor Fedorenko <fedore.egor@gmail.com>
2023-07-24 16:54:05 +0300

    adding first values

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO regions (uuid, region_name, region_short_name) VALUES (1, 'Leningradskaya oblast', 'LO');
+INSERT INTO regions (uuid, region_name, region_short_name) VALUES (2, 'Moskovskaya oblast', 'MO');
+INSERT INTO regions (uuid, region_name, region_short_name) VALUES (3, 'Primorskiy kray', 'PK');
