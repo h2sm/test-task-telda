2f5c2f8ac82bcc6ac1f42789fe13c1cfa5398cf7
Egor Fedorenko <fedore.egor@gmail.com>
2023-07-24 16:45:53 +0300

    Init commit

---
+++ src/main/java/com/h2sm/testtasktelda/TestTaskTeldaApplication.java
@@ -0,0 +1,13 @@
+package com.h2sm.testtasktelda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskTeldaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestTaskTeldaApplication.class, args);
+    }
+
+}
